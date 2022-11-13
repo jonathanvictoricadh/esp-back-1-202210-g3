9a35a59b91c47422b4f7e3e53334f7bb7c3f9a5b
Jonathan Victorica <jonathan.m.victorica@hotmail.com>
2022-11-13 15:55:11 -0300

    init

---
+++ api-playlist/src/main/java/com/spotify/playlist/ApiPlaylistApplication.java
@@ -0,0 +1,18 @@
+package com.spotify.playlist;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.openfeign.EnableFeignClients;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+
+@SpringBootApplication
+@EnableJpaRepositories
+@EnableFeignClients
+public class ApiPlaylistApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ApiPlaylistApplication.class, args);
+    }
+
+}
